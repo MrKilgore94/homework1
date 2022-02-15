8dc7e945821fe98ee37eca1ca3480a32ef732cf5
Austin Kilgore <mr.kilgore@Austins-MacBook-Air.local>
2022-02-15 13:09:15 -0700

    added notes

---
+++ objects.js
@@ -0,0 +1,20 @@
+// objects are key value pairstore
+//kind of like describing a person
+
+
+let dict = {apple: 'a thing that grows on a tree', zebra: 'a striped animal'} 
+
+
+// keys
+let bob = {name: 'bob', age:21}
+let jill = {name: 'Jill', age: 24}
+
+//adding another key
+bob.eyeColor = 'Blue'
+
+//access key
+console.log(bob.name)
+console.log(bob.age)
+
+let people = [bob, jill]
+console.log(people[1].name)
