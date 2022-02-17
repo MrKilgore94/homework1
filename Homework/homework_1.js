3b5e391a9b5c72643644a499bc491f13af0ae778
Austin Kilgore <mr.kilgore@Austins-MacBook-Air.local>
2022-02-16 23:43:28 -0700

    added into homework_2

--- Homework/homework_1.js
+++ Homework/homework_1.js
@@ -1,6 +1,10 @@
 // ~~~~~~~~~~~Loops~~~~~~~~~~~~~~~
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Loops~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
 
 // 1. Write a for loop that logs only logs even numbers from 0 - 10; ie 0,2,4,..10
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Question 1~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
 for(let i = 0; i <=10; i++){
   if( i % 2 === 0){
   console.log(`${i} is even`)
@@ -10,6 +14,8 @@ for(let i = 0; i <=10; i++){
 //--------------------------------------------------------------------------------
 
 // 2. Write a for that logs numbers from 10 - 0; ie 10,9,8...0
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Question 2~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
 for (let i=20; i>=1; i--) {
   console.log(i);
 }
@@ -17,6 +23,8 @@ for (let i=20; i>=1; i--) {
 //--------------------------------------------------------------------------------
 
 // 3. write a for loop that loops through this array and logs 'odd/even' based on wether the number is odd even
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Question 3~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
 for (var x = 0; x <= 20; x++){
   if (x === 0){
     console.log(x + " is even");
@@ -34,6 +42,8 @@ for (var x = 0; x <= 20; x++){
 
 
 // 4. write a while loop that starts at 0 and counts up to 10 ie: 0,1,2,3....10
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Question 4~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
 
 let i = 0;
 
@@ -47,6 +57,8 @@ while (i <= 10) {
 // 6. what will this log? 
 // 7. what is the value of isOdd(2) 
 // 8. Fix the isOdd function to make this work
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Questions 5,6,7,8~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
 
 
 const isOdd = (num) =>{
@@ -60,9 +72,13 @@ if(isOdd(num ='3648392')){
 }
 //--------------------------------------------------------------------------------
 // ~~~~~~~Functions~~~~~~~~~
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Functions~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 
 // 1. Write a function that takes a positive number and prints all even numbers from 0 – number
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Question 1~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+
 
 const printUpTo = (num)=>{
   for(let i = 0; i<= num; i++){
@@ -78,6 +94,8 @@ printUpTo(30)
 //--------------------------------------------------------------------------------
 
 // 2. Write a function that takes an array and returns the sum of all the numbers
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Question 2~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
 const array = [1, 3, 5, 6, 9];
 let sum = 0;
 
@@ -88,6 +106,8 @@ console.log(sum);
 //--------------------------------------------------------------------------------
 
 // 3. Write a function that returns the number of vowels in string
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Question 3~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
 var s = "How many vowels are in this sentence";
 
 let count2 = s.split('').filter( char => /[aeiou]/i.test(char) ).length;
@@ -96,6 +116,7 @@ console.log("How many vowels are in this sentence " + count2)
 //--------------------------------------------------------------------------------
 
 // 4. Write a function that takes a math operator, as a string, ('*','/','+','-') and two numbers and returns the result
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Question 4~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 
 
@@ -103,12 +124,15 @@ console.log("How many vowels are in this sentence " + count2)
 //--------------------------------------------------------------------------------
 
 // 5. Write a function that takes an array of strings and a string and returns true or false if the string is in the array.
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Question 5~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 
 
 //--------------------------------------------------------------------------------
 
 // 6. write a function that takes a person object {firstname: 'joe', lastname: 'shmoe', age: 50} and returns a formatted string "joe shmoe is 50 years old"
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Question 6~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
 
 
 let frank = {name: "Frank Castle", age: 40};
@@ -129,12 +153,31 @@ console.log(lara.name + " is " + lara.age + " years old!");
 //--------------------------------------------------------------------------------
 
 // 7. write a function that takes an array that removes the first and last items and return that new array
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Question 7~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
+let remove1 = (arr)=>{
+  const cloneArr = [...arr]
+  shift(cloneArr)
+  return cloneArr
+}
+
+let remove2 = (arr)=>{
+  const cloneArr = [...arr]
+  pop(cloneArr)
+  return cloneArr
+}
+
+let nums = [2,3,4,5]
+let swapedNums = swap1(nums)
+console.log('nums: ', nums)
+console.log('swapedNums: ', swapedNums)
 
 
 //--------------------------------------------------------------------------------
 
 // 8. write a function that swaps the last and first items of an array 
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Question 8~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
 const swap1 = (arr)=>{
   const cloneArr = [...arr]
   swap(cloneArr)
@@ -151,20 +194,30 @@ console.log('swapedNums: ', swapedNums)
 //--------------------------------------------------------------------------------
 
 // 9. write a function that takes a array and a second argument and returns the index in the array of the second argument
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Question 9~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 
 
 //--------------------------------------------------------------------------------
 
 // 10. write a function that takes a number and uses a ternary to return if the number is odd or even.
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Question 10~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 
 
 //--------------------------------------------------------------------------------
 
 // 11. Write a function that prints all  numbers from 0 – 10
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Question 11~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 
+const UpTo10 = (num)=>{
+  for(let i = 0; i<= num; i++){
+      console.log(i)
+      }
+  }
+
+UpTo10(10)
 
 //--------------------------------------------------------------------------------
 
